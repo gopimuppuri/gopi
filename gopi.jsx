75ca21fb3d3bd03a5e3c615bf2e65ceeb087794e
gopimuppuri <127486145+gopimuppuri@users.noreply.github.com>
2023-11-02 21:23:49 +0530

    Add files via upload

---
+++ gopi.jsx
@@ -0,0 +1,33 @@
+export function Users() 
+{
+    let name="gopi";
+    let email = "gopi@gmail.com";   
+    let phone = 9133662569; I      
+    let gender = "Male";      
+    let dob = "04/05/1998";
+    return <>
+    <div className="margin-20">
+        <div className="margin-10"> 
+        <label>Name:</label>
+        <label>gopii</label>
+        </div>
+        <div className="margin-10">    
+        <label>Email:</label> 
+        <label>gopi@gmail.com</label>   
+        </div>  
+        <div className="margin-10">
+        <label>Phone:</label>
+        <label>9133662569</label>    
+        </div>
+        <div className="margin-10">   
+        <label>Gender:</label>
+        <label>Male</label>
+        </div>
+        <div className="margin-10">
+        <label>DOB:</label>
+        <label>04/05/1998</label>
+        </div>
+    </div>
+
+   </>
+}    
